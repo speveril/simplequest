822cdcb79d3d5d89b7dcc6baf5ae66d47b0786cb
Shamus Peveril <speveril@gmail.com>
2018-04-01 11:46:46 -0700

    Initial commit. Working on porting to post 0.1.0 Cozy and Lotus (aka newer RPGKit)

---
+++ src/menus/all.ts
@@ -0,0 +1,14 @@
+import * as Cozy from 'Cozy';
+import * as RPG from '../../../lotus/core/Lotus';
+
+export * from './Boot';
+export * from './GameOver';
+export * from './Main';
+export * from './Shop';
+
+export function quitGame() {
+    RPG.Scene.do(function*() {
+        yield* RPG.Scene.waitFadeTo("black", 1.0);
+        Cozy.quit();
+    }.bind(this));
+}
