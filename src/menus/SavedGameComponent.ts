3536f56bdf3028ee7eeff31e8a5c18a22e44dd01
Shamus Peveril <speveril@gmail.com>
2018-04-13 21:16:42 -0700

    Fix display of saved game images.

--- src/menus/SavedGameComponent.ts
+++ src/menus/SavedGameComponent.ts
@@ -7,7 +7,7 @@ export class SavedGameComponent extends Cozy.UiComponent {
             className: 'saved-game',
             html: `
                 <div class="image">
-                    <img src="${args.img || ''}">
+                    <img src="">
                 </div>
                 <div class="info">
                     <div class="game-name">${args.name}</div>
@@ -17,6 +17,12 @@ export class SavedGameComponent extends Cozy.UiComponent {
                 </div>
             `
         });
+        
+        // For whatever reason, just outputting the <img> tag with the proper src
+        // doesn't work consistently (on Macs); might be some sort of limitation
+        // on the length of a src URL? Anyway, setting the attribute directly
+        // seems to work.
+        this.find('.image img').setAttribute('src', args.img);
         this.element.setAttribute('data-id', args.id);
         this.element.setAttribute('data-menu', 'choose');
     }
